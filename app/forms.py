5f49ecc8f104b3edabffbcff44157dc1c29abece
Dwight Townsend <dwtownse@calpoly.edu>
2014-09-03 18:44:03 -0700

    Chapter 11, Group all errors together

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,7 @@
 from flask_wtf import Form
 from wtforms import TextField, DateField, IntegerField, \
     SelectField, PasswordField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 
 class AddTaskForm(Form):
@@ -31,15 +31,15 @@ class RegisterForm(Form):
     )
     email = TextField(
         'Email',
-        validators=[DataRequired(), Length(min=6, max=40)]
+        validators=[DataRequired(), Email(), Length(min=6, max=40)]
     )
     password = PasswordField(
         'Password',
-        validators=[DataRequired(), Length(min=5, max=40)]
+        validators=[DataRequired(), Length(min=6, max=40)]
     )
     confirm = PasswordField(
         'Repeat Password',
-        [DataRequired(), EqualTo('password', message='Passwords must match')]
+        validators=[DataRequired(), EqualTo('password', message='Passwords must match')]
     )
 
 
