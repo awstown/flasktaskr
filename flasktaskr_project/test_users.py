2f8b0fafe2d426d4f0241b756f4be2ee163b1732
Dwight Townsend <townsenddw@gmail.com>
2014-10-08 11:10:59 -0700

    Error Handling

--- flasktaskr_project/test_users.py
+++ flasktaskr_project/test_users.py
@@ -199,6 +199,26 @@ class UsersTests(unittest.TestCase):
         for user in users:
             self.assertEquals(user.role, 'user')
 
+    ################
+    #### errors ####
+    ################
+    def test_404_error(self):
+        response = self.app.get('/this-route-does-not-exist')
+        self.assertEquals(response.status_code, 404)
+        self.assertIn('Sorry. There\'s nothing here.', response.data)
+
+    def test_500_error(self):
+        bad_user = User(
+            name='Jeremy',
+            email='jeremy@realpython.org',
+            password='django'
+        )
+        db.session.add(bad_user)
+        db.session.commit()
+        response = self.login('Jeremy', 'django')
+        self.assertEquals(response.status_code, 500)
+        self.assertNotIn('ValueError: Invalid salt', response.data)
+        self.assertIn('Something went terribly wrong.', response.data)
 
 if __name__ == "__main__":
     unittest.main()
